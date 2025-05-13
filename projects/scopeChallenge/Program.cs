2d99f1a417055c9e1350da6981c7ae5c4c294d27
Gaya <91261240+sdooodly@users.noreply.github.com>
2025-05-13 06:50:55 +0000

    thrid

---
+++ projects/scopeChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+bool found = false;
+int total=0;
+foreach (int number in numbers)
+{
+    
+
+    total += number;
+
+    if (number == 42) found = true;
+    
+
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
