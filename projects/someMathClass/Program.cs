4bfcef87089afe9fdd3016dc4baefa52c8850372
Gaya <91261240+sdooodly@users.noreply.github.com>
2025-05-13 04:37:25 +0000

    oru easy challenge

---
+++ projects/someMathClass/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
